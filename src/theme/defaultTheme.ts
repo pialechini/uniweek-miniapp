e11eea1d5621038d1bed262a0fe45e696cc1d4a4
pialechini <mobinzohourian@gmail.com>
2024-12-03 18:25:35 +0330

    make project clean for a new beginning

--- src/theme/defaultTheme.ts
+++
@@ -1,8 +0,0 @@
-export const defaultTheme = {
-  colors: {
-    pageBackground: "#000B0F",
-    primary: "#2986A3",
-    accent: "#64CCC5",
-    gray: "#8EA2A1",
-  },
-};
--- src/types/styled.d.ts
+++
@@ -1,8 +0,0 @@
-import { defaultTheme } from "@/theme/defaultTheme.ts";
-import "styled-components";
-
-type Theme = typeof defaultTheme;
-
-declare module "styled-components" {
-  export interface DefaultTheme extends Theme {}
-}
