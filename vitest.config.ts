aac223fbd1af87baf82f8284bf57fff0c83cc39c
pialechini <mobinzohourian@gmail.com>
2024-12-11 20:40:37 +0330

    chore: add testing ability to the app chore: install date-fns

---
+++ vitest.config.ts
@@ -0,0 +1,17 @@
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    ],
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+});
