970cf6e4830bec5653808ac01b954b16a18a9064
pialechini <mobinzohourian@gmail.com>
2024-12-11 20:42:34 +0330

    feat: add ability to detect even/odd weeks

--- src/helpers/date.ts
+++ src/helpers/date.ts
@@ -1,4 +1,5 @@
 import { Weekday } from '@/types';
+import { differenceInDays, isSaturday, previousSaturday } from 'date-fns';
 
 function formatPersianDate(date: Date) {
   return date.toLocaleDateString('fa-IR', {
@@ -12,6 +13,20 @@ function toWeekday(date: Date) {
   return date.toLocaleDateString('fa-IR', {
     weekday: 'long',
   }) as Weekday;
+}
+
+export function getWeekParity(today: Date, startOfTerm: Date): 'even' | 'odd' {
+  const saturdayOf = (date: Date) =>
+    isSaturday(date) ? date : previousSaturday(date);
+
+  const firstSaturdayInTheTerm = saturdayOf(startOfTerm);
+  const saturdayOfTheWeek = saturdayOf(today);
+
+  const weekNumber = Math.floor(
+    differenceInDays(saturdayOfTheWeek, firstSaturdayInTheTerm) / 7 + 1,
+  );
+
+  return weekNumber % 2 === 0 ? 'even' : 'odd';
 }
 
 export { formatPersianDate, toWeekday };
