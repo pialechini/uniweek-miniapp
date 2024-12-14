0da8998abb4dedc8bd01cc4fb943df4f91666d77
pialechini <mobinzohourian@gmail.com>
2024-12-15 01:06:14 +0330

    feat: display app version in LoadingPage

--- src/pages/LoadingPage.tsx
+++ src/pages/LoadingPage.tsx
@@ -1,3 +1,4 @@
+import packageJson from '@/../package.json';
 import UniweekSvg from '@/components/logos/UniweekLogo';
 
 import styles from './loadingPage.module.scss';
@@ -7,10 +8,11 @@ function LoadingPage() {
     <div className={styles.bg}>
       <div className={styles.upBlur}></div>
       <div className={styles.downBlur}></div>
-      <div className={styles.wrapper}>
-        <UniweekSvg className={styles.logo} svgClassName={styles.svgDrawing}>
-          <h1 className={styles.text}>Uniweek</h1>
-        </UniweekSvg>
+
+      <div className={styles.column}>
+        <UniweekSvg size="280px" />
+        <h1 className={styles.text}>Uniweek</h1>
+        <span className={styles.version}>v{packageJson.version}</span>
       </div>
     </div>
   );
